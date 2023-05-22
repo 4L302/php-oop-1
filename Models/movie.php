8bfa0e6e63d41a8b4203d875f2878a5b70ef645f
4L302 <alegarg2002@gmail.com>
2023-05-22 17:47:58 +0200

    .

--- Models/movie.php
+++ Models/movie.php
@@ -1,18 +1,25 @@
 <?php
-  class Movie {
-    public $id;
-    public $title;
-    public $original_title;
-    public $nationality;
-    public $date;
-    public $vote;
-    public $image;
+include __DIR__ . '/Genre.php';
+class Movie
+{
+  public $title, $poster, $duration, $language, $genre;
 
-    public function __construct($title, $original_title, $nationality, $date, $vote){
-       $this->title = $title;
-       $this->original_title = $original_title;
-       $this->nationality = $nationality;
-       $this->date = $date;
-       $this->vote = $vote;
-    }
+  public function __construct(string $title = '', string $poster = '', int $duration = 180, string $language = 'en', Genre $genre)
+  {
+    $this->title = $title;
+    $this->poster = $poster;
+    $this->duration = $duration;
+    $this->language = $language;
+    $this->genre = $genre;
   }
+
+  public function getDetails(){
+    
+    return "Duration: $this->duration Language: $this->language Genre: {$this->genre->name}";
+  }
+}
+
+$movie = new Movie('Il Padrino', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/r4gnMXuY1efvaolNDnj4046S.jpg', 120, 'en', new Genre('Drammatico'));
+var_dump($movie);
+
+echo $movie->getDetails();
--- partials/index.php
+++ partials/index.php
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Movies</h1>
-    <div class="row">
-        <div class="col-12 col-sd-6 col-md-4 col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-title">
-                        <?php
-                        echo $title->title
-                        ?>
-                    </div>
-                    <div class="card-text">
+<?php
+session_start();
+if (!isset($_SESSION['userId'])){
+    if (ini_get("session.use_cookies")){
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();    
+}
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+?>
 
-</div>
-</body>
-</html>
+<h1>Movies</h1>
